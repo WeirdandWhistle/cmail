40402b425b383794a901ec94eed72b0e1619e4dd
whynotjava <tinycreeperx@gmail.com>
2026-03-28 14:24:47 -0400

    alert system

--- static/app.js
+++ static/app.js
@@ -11,9 +11,27 @@ const signin_password = document.getElementById("signin-password");
 const signin_username = document.getElementById("signin-username");
 const signup_password = document.getElementById("signup-password");
 const signup_username = document.getElementById("signup-username");
+const alerts = document.getElementById("alerts");
 
 async function main(){
     await sodium.ready;
     console.log("from main",sodium.to_hex(sodium.crypto_hash_sha256("test")));
     console.log("sodium loaded and ready to go!");
+}
+
+let count = 1;
+function alert(text,color){
+    const alertTime = 5;
+    const element = `<div id="alert-${count}" style="height: min-content; width: 100%;color: ${color};border: 1px ${color} solid; padding: 2px;">${text}</div>`;
+
+    alerts.insertAdjacentHTML("afterbegin",element);
+
+    setTimeout(()=>{document.getElementById(`alert-${count}`).remove();},alertTime*1000);
+}
+
+function signin(){
+    alert("for no reason","green");
+}
+function signup(){
+
 }
